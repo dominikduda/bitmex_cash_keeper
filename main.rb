d9f9018d7c9c287c19c818bdeeb6aa43baa844da
dominikduda <gospodorz@gmail.com>
2019-03-12 20:50:17 +0100

    Retry in place on every request error

--- main.rb
+++ main.rb
@@ -29,43 +29,48 @@ def countdown
   10.times { |i| print 10 - i; print ' '; sleep 1  }
 end
 
+def safe_response(&request)
+  response = nil
+  loop do
+    begin
+      response = request.call
+    rescue BitmexError => error
+      puts "\tThere was an error:"
+      puts error
+      sleep 3
+      next
+    rescue Faraday::ConnectionFailed
+      puts "\tCould not connect to Bitmex"
+      sleep 3
+      next
+    end
+    return response if response&.status == 200
+  end
+end
+
 script_start_timestamp = Time.now.strftime('%d-%m-%Y %H:%M:%S')
 detected_closes = 0
 
 loop do
-  begin
-    system 'clear'
-    puts "\t  Script started at:\t#{script_start_timestamp}"
-    puts "\t    Detected closes:\t#{detected_closes}"
-    puts
-    response = private_client.user_margin
-    user_margin = response.body
-    free_balance = to_xbt(user_margin.fetch(:availableMargin))
-    total_amount = to_xbt(user_margin.fetch(:walletBalance))
-    if (free_balance == total_amount)
-      puts "POSITION CLOSE DETECTED"
-      detected_closes += 1
-      loop do
-        last_price = public_client.instrument({ symbol: 'XBTUSD' }).body.first[:lastPrice]
-        order_quantity = (free_balance * last_price).round(0) - 10
-        response = private_client.create_order('XBTUSD', order_quantity, side: 'Sell', ordType: 'Market')
-        next if response.status != 200
-        break
-      end
-      loop do
-        response = private_client.position_isolate('XBTUSD', false)
-        next if response.status != 200
-        break
-      end
-      puts "ENTERED SHORT x0 WITH WHOLE ACCOUNT"
-    end
-    puts "\t      Last check at:\t#{Time.now.strftime('%d-%m-%Y %H:%M:%S')}"
-    puts "\t Remaining requests:\t#{response.headers['x-ratelimit-remaining']}/#{response.headers['x-ratelimit-limit']}"
-    puts "\t       Free balance:\t#{formatted_balance(free_balance)}"
-    puts "\tOn exchange balance:\t#{formatted_balance(total_amount)}"
-    countdown
-  rescue Faraday::ConnectionFailed
-    puts "\tCould not connect to Bitmex"
-    countdown
+  system 'clear'
+  puts "\t  Script started at:\t#{script_start_timestamp}"
+  puts "\t    Detected closes:\t#{detected_closes}"
+  puts
+  user_margin_info = safe_response { private_client.user_margin }
+  free_balance = to_xbt(user_margin_info.body.fetch(:availableMargin))
+  total_amount = to_xbt(user_margin_info.body.fetch(:walletBalance))
+  if (free_balance == total_amount)
+    puts "\tPOSITION CLOSE DETECTED"
+    detected_closes += 1
+    last_price = safe_response { public_client.instrument({ symbol: 'XBTUSD' }) }.body.first[:lastPrice]
+    order_quantity = (free_balance * last_price).round(0) - 10
+    safe_response { private_client.create_order('XBTUSD', order_quantity, side: 'Sell', ordType: 'Market') }
+    safe_response { private_client.position_isolate('XBTUSD', false) }
+    puts "\tENTERED SHORT x0 WITH WHOLE ACCOUNT"
   end
+  puts "\t      Last check at:\t#{Time.now.strftime('%d-%m-%Y %H:%M:%S')}"
+  puts "\t Remaining requests:\t#{user_margin_info.headers['x-ratelimit-remaining']}/#{user_margin_info.headers['x-ratelimit-limit']}"
+  puts "\t       Free balance:\t#{formatted_balance(free_balance)}"
+  puts "\tOn exchange balance:\t#{formatted_balance(total_amount)}"
+  countdown
 end
