6251264197c221fbf98aeb4649a7b4f99bcb209a
dominikduda <gospodorz@gmail.com>
2019-07-21 00:15:04 +0200

    Fetching data for shorting future ready

--- main.rb
+++ main.rb
@@ -15,6 +15,8 @@ end
 private_client = Bitmex.http_private_client(ENV['BITMEX_API_KEY'], ENV['BITMEX_API_SECRET'])
 public_client = Bitmex.http_public_client
 
+FUTURE_SETTLING_ON_CURRENCY_TYPE = 'FFCCSX'.freeze
+
 def to_xbt(amount)
   (BigDecimal(amount, 10) * 0.00000001).to_f
 end
@@ -50,12 +52,32 @@ end
 
 script_start_timestamp = Time.now.strftime('%d-%m-%Y %H:%M:%S')
 detected_closes = 0
+latest_expiring_xbt_future = nil
 
 loop do
   system 'clear'
-  puts "\t  Script started at:\t#{script_start_timestamp}"
-  puts "\t    Detected closes:\t#{detected_closes}"
-  puts
+  loop do
+    break if latest_expiring_xbt_future
+    puts "\tFinding latest expiring XBT future..."
+    this_and_next_year_suffixes = [Time.now.year, Time.now.year + 1].map do |year|
+      year.to_s.chars.last(2).join
+    end
+    xbt_month_futures = safe_response do
+      public_client.instrument(filter: { rootSymbol: 'XBT', typ: FUTURE_SETTLING_ON_CURRENCY_TYPE })
+    end&.body&.select do |future|
+      this_and_next_year_suffixes.include?(future[:symbol].chars.last(2).join)
+    end
+    if xbt_month_futures&.none?
+      puts "\tFailure, retrying..."
+      next
+    end
+    latest_expiring_xbt_future = xbt_month_futures.sort_by do |future|
+      Time.new(future[:expiry])
+    end.last
+    puts "\tSuccess!"
+    puts
+    latest_expiring_xbt_future
+  end
   user_margin_info = safe_response { private_client.user_margin }
   free_balance = to_xbt(user_margin_info.body.fetch(:availableMargin))
   total_amount = to_xbt(user_margin_info.body.fetch(:walletBalance))
@@ -68,8 +90,14 @@ loop do
     safe_response { private_client.position_isolate('XBTUSD', false) }
     puts "\tENTERED SHORT x0 WITH WHOLE ACCOUNT"
   end
+  puts "\t  Script started at:\t#{script_start_timestamp}"
   puts "\t      Last check at:\t#{Time.now.strftime('%d-%m-%Y %H:%M:%S')}"
+  puts "\t    Detected closes:\t#{detected_closes}"
+  puts
   puts "\t Remaining requests:\t#{user_margin_info.headers['x-ratelimit-remaining']}/#{user_margin_info.headers['x-ratelimit-limit']}"
+  puts "\tFound future symbol:\t#{latest_expiring_xbt_future[:symbol]}"
+  puts "\t Future expiry date:\t#{Time.new(latest_expiring_xbt_future[:expiry]).strftime('%d-%m-%Y %H:%M:%S')}"
+  puts
   puts "\t       Free balance:\t#{formatted_balance(free_balance)}"
   puts "\tOn exchange balance:\t#{formatted_balance(total_amount)}"
   countdown
